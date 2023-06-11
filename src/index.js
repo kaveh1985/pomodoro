6a6bb554b538c5e5936f9087e740ed2b48def158
Ramzanzad Malek <kaveh.ramzanzad@gmail.com>
2023-06-11 02:27:53 +0200

    latest updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Modal from 'react-modal';
 import App from "./App";
 import "./index.css";
 
-Modal.setAppElement('#root');
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App/>)
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
